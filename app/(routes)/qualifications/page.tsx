46f9f4baeab0030912e884df5a9ac27ee5ebc768
jona1995 <43344575+jona1995@users.noreply.github.com>
2025-01-11 16:03:08 -0300

    ajuste

--- app/(routes)/qualifications/page.tsx
+++ app/(routes)/qualifications/page.tsx
@@ -1,10 +1,8 @@
-import { HeaderQualifications } from './components/HeaderQualifications';
 import { Qualifications } from './components/ListQualifications';
 
 export default function QualificationsPage() {
 	return (
 		<div>
-			<HeaderQualifications />
 			<Qualifications />
 		</div>
 	);

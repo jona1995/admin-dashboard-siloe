d8c061bdc2dfc1db4c4c614c3b9e22929fde40eb
jona1995 <43344575+jona1995@users.noreply.github.com>
2025-01-11 16:20:12 -0300

    prueba error

--- app/(routes)/enrollments/[enrollmentId]/components/Enrollmentnformation/EnrollmentInformation.types.ts
+++ app/(routes)/enrollments/[enrollmentId]/components/Enrollmentnformation/EnrollmentInformation.types.ts
@@ -1,5 +1,13 @@
 import { Enrollment } from '@prisma/client';
 
+// export type EnrollmentInformationProps = {
+// 	enrollment: Enrollment;
+// };
+
+type EnrollmentWitCourses = Enrollment & {
+	courses: { id: number; nombre: string }[];
+};
+
 export type EnrollmentInformationProps = {
-	enrollment: Enrollment;
+	enrollment: EnrollmentWitCourses;
 };
--- app/(routes)/payments/[paymentId]/components/PaymentInformation/PaymentInformation.types.ts
+++ app/(routes)/payments/[paymentId]/components/PaymentInformation/PaymentInformation.types.ts
@@ -1,5 +1,13 @@
 import { Payment } from '@prisma/client';
 
+// export type PaymentInformationProps = {
+// 	payment: Payment;
+// };
+
+type PaymentWithBeneficiarios = Payment & {
+	beneficiarios: { id: number; nombre: string }[];
+};
+
 export type PaymentInformationProps = {
-	payment: Payment;
+	payment: PaymentWithBeneficiarios;
 };

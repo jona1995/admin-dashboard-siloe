223fbf53a97e5479065951027670fbf92b924e00
jona1995 <43344575+jona1995@users.noreply.github.com>
2025-01-30 16:37:19 -0300

    .

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,6 +1,6 @@
 import { Sidebar } from '@/components/Sidebar';
 import GlobalLoader from './globalLoader/globalLoader';
-import { Navbar } from '@/components/Navbar';
+import { Navbar } from '@/components/Navbar/NavBar';
 
 export default function LayoutDashboard({
 	children,

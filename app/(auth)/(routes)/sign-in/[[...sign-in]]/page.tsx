65bbd1dde79455c062fb7e06c88aa467a8e3dd37
jona1995 <43344575+jona1995@users.noreply.github.com>
2025-02-03 19:57:42 -0300

    ajuste sesion

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,28 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignIn } from '@clerk/nextjs';
 
-export default function Page() {
-    return <SignIn path="/sign-in" />;
-}
+// pages/signin.tsx
+import { useUser } from '@clerk/clerk-react';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const Page = () => {
+	const { user } = useUser(); // Obtén el estado del usuario
+	const router = useRouter();
+
+	useEffect(() => {
+		if (user) {
+			// Redirigir si el usuario ya está autenticado
+			router.push('/'); // Cambia '/home' por la página de inicio o la página que prefieras
+		}
+	}, [user, router]);
+
+	if (user) return null; // No renderizar el formulario si el usuario ya está autenticado
+
+	return (
+		<div>
+			<SignIn path="/sign-in" />;
+		</div>
+	);
+};
+
+export default Page;

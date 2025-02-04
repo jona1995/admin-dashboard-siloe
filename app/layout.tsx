3edbd3a8d7e00ae50b03fb2f8244986d734d3c95
jona1995 <43344575+jona1995@users.noreply.github.com>
2025-02-04 00:39:06 -0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,67 +1,37 @@
-import type { Metadata } from 'next';
-import { ClerkProvider } from '@clerk/nextjs';
-import { ThemeProvider } from '@/components/theme-provider';
-import { Toaster } from '@/components/ui/toaster';
+import type { Metadata } from "next";
+import { Noto_Sans_Display } from "next/font/google";
+import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/toaster"
 
-import './globals.css';
+import "./globals.css";
+
+const noto = Noto_Sans_Display({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: 'Dashboard Admin | Siloe',
-	metadataBase: new URL('http://localhost:3000'),
-	description: 'Seminario Siloe ',
-	keywords: 'dashboard, administración, seminario, Siloe',
-	openGraph: {
-		title: 'Dashboard Admin | Siloe',
-		description: 'Gestión administrativa del seminario Siloe',
-		url: 'https://siloe.com.uy',
-		siteName: 'Dashboard Siloe',
-		images: [
-			{
-				url: '/logo_siloe.png',
-				width: 1200,
-				height: 630,
-				alt: 'Imagen del Dashboard Siloe',
-			},
-		],
-		locale: 'es_ES',
-		type: 'website',
-	},
-	// twitter: {
-	// 	card: 'summary_large_image',
-	// 	title: 'Dashboard Admin | Siloe',
-	// 	description: 'Gestión administrativa del seminario Siloe',
-	// 	images: ['/twitter-image.png'],
-	// },
+  title: "Dashboard Companies | TarreDev",
+  description: "Course Tarredev ",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<ClerkProvider>
-			<html lang="en">
-				<head>
-					<link
-						rel="icon"
-						href="/favicon.ico"
-						sizes="16x16"
-						type="image/x-icon"
-					/>
-					<meta name="viewport" content="width=device-width, initial-scale=1" />
-				</head>
-				<body>
-					<ThemeProvider
-						attribute="class"
-						enableSystem
-						disableTransitionOnChange
-					>
-						{children}
-						<Toaster />
-					</ThemeProvider>
-				</body>
-			</html>
-		</ClerkProvider>
-	);
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={noto.className}>
+          <ThemeProvider
+            attribute="class"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster />
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
